7a18175118a182057733bda3dc861f3bc8d1575a
Selim <selimx2001x@gmail.com>
2025-09-24 14:35:39 +0300

    Implemented ExcelUtils.java class to fetch data from the excel file and added required dependencies

--- src/main/java/utils/ExcelUtils.java
+++ src/main/java/utils/ExcelUtils.java
@@ -1,4 +1,26 @@
 package utils;
+import org.apache.poi.ss.usermodel.*;
+import java.io.FileInputStream;
 
 public class ExcelUtils {
+    public static Object[][] getData(String filePath, String sheetName) throws Exception {
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook wb = WorkbookFactory.create(fis);
+        Sheet sheet = wb.getSheet(sheetName);
+
+        int rows = sheet.getPhysicalNumberOfRows();
+        int cols = sheet.getRow(0).getPhysicalNumberOfCells();
+
+        Object[][] data = new Object[rows - 1][cols];
+
+        for (int i = 1; i < rows; i++) {   // skip header
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < cols; j++) {
+                data[i - 1][j] = row.getCell(j).toString();
+            }
+        }
+        wb.close();
+        fis.close();
+        return data;
+    }
 }
