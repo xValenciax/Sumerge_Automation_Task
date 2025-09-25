16653742f997168502651c22fcb02e0911d6241a
Selim <selimx2001x@gmail.com>
2025-09-26 02:12:22 +0300

    - Added one extra sheet in TestData excel file - Modified date picking to be data-driven

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,18 +6,19 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.time.Month;
 import java.util.regex.Pattern;
 
 public class HomePage {
     By CitySelector = By.id(":rh:");
     By FirstSearchResult = By.cssSelector("#autocomplete-result-0 > div");
     By CalendarBtn = By.id("calendar-searchboxdatepicker-tab-trigger");
-    By MonthName = By.xpath("/html/body/div[3]/div[2]/div/form/div/div[2]/div/div/div/nav/div[2]/div/div[1]/div/div[1]/h3");
-    By NextMonthBtn = By.cssSelector("#calendar-searchboxdatepicker > div > div.a2142b454f.fb6f2a3ebc > button");
-    By CheckInDay;
-    By CheckOutDay;
+    By MonthName = By.xpath("//*[@id=\"calendar-searchboxdatepicker\"]/div/div[1]/div/div[1]/h3");
+    By NextMonthBtn = By.cssSelector("button[aria-label=\"Next month\"]");
     By SearchBtn = By.cssSelector("button[type=submit]");
     By SearchResultCount = By.xpath("/html/body/div[4]/div/div/div/div[2]/div[3]/div[2]/div[1]/h1");
+    By CheckInDay;
+    By CheckOutDay;
 
 
     private WebDriverWait wait;
@@ -41,10 +42,17 @@ public class HomePage {
     }
 
     public void selectDate(int year, int month, int checkInDay, int checkOutDay) {
-        driver.findElement(NextMonthBtn).click();
 
-        CheckInDay = By.cssSelector("span[data-date='" + String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.format("%02d", checkInDay) + "']");
-        CheckOutDay = By.cssSelector("span[data-date='" + String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.format("%02d", checkOutDay) + "']");
+        String MonthValue = Month.of(month).getDisplayName(java.time.format.TextStyle.FULL, java.util.Locale.ENGLISH);
+
+        while (!driver.findElement(MonthName).getText().contains(MonthValue)) {
+            driver.findElement(NextMonthBtn).click();
+        }
+
+        CheckInDay = By.cssSelector("span[data-date='" + String.valueOf(year) + "-" +
+                String.format("%02d", month) + "-" + String.format("%02d", checkInDay) + "']");
+        CheckOutDay = By.cssSelector("span[data-date='" + String.valueOf(year) + "-" +
+                String.format(String.format("%02d", month)) + "-" + String.format("%02d", checkOutDay) + "']");
 
         driver.findElement(CheckInDay).click();
         driver.findElement(CheckOutDay).click();
